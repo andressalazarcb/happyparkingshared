1390117112402664a39b538068a37d165052596e
Andres <andres.salazar@ceiba.com.co>
2019-04-05 13:40:21 -0500

    base

---
+++ src/main/java/co/com/ceiba/estacionamiento/andres/salazar/happyparkingshared/HappyparkingsharedApplication.java
@@ -0,0 +1,13 @@
+package co.com.ceiba.estacionamiento.andres.salazar.happyparkingshared;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyparkingsharedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyparkingsharedApplication.class, args);
+	}
+
+}
